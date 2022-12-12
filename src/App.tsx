49075033d38d0fc02045b3aa2b1439e45dfb9857
kayque-willy@hotmail.com <kayque-wily~@hotmail.com>
2022-12-12 15:23:08 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import { Tweet } from "./components/Tweet"
+
 function App() {
-    return <h1>Hello World!</h1>
+    const [tweets, setTweets] = useState<string[]>([
+        'Tweet 1',
+        'Tweet 2',
+        'Tweet 3',
+        'Tweet 4'
+    ]);
+
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            {/* <Tweet text="Texto do Tweet 1" title="Título do Tweet 1" />
+            <Tweet text="texto do Tweet 2" title="Título do Tweet 2" />
+            <Tweet text="Texto do Tweet 3" /> */}
+            {tweets.map(tweet => {
+
+                return <Tweet text={tweet} />
+
+            })}
+
+            <button>Adicionar Tweet</button>
+        </div>
+    );
 }
 
 export default App
---
+++ src/components/Tweet.tsx
@@ -0,0 +1,15 @@
+type TweetProps = {
+    title?: string
+    text: string;
+}
+
+export function Tweet(props: TweetProps) {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <p>{props.text}</p>
+            <p>Texto adicional...</p>
+            <hr></hr>
+        </div>
+    );
+}
