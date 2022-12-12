1c88cdfef90df24edef89843c2f767282e59d16f
kayque-willy@hotmail.com <kayque-willy@hotmail.com>
2022-12-12 16:08:44 -0300

    melhorias

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import { useState } from "react";
 import { Tweet } from "./components/Tweet";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   // Cria um array de string que representa os tweets
-  const [tweets, setTweets] = useState<string[]>([
-    "Tweet 1",
-    "Tweet 2",
-    "Tweet 3",
-    "Tweet 4",
-  ]);
+  const [tweets, setTweets] = useState<string[]>(["Tweet 1", "Tweet 2"]);
+  const [tweetNumber, setTweetNumber] = useState(3);
 
   function createTweet() {
     // O setState apaga o antigo estado e adiciona um novo
     //Copia os tweets anteriores e adiciona o novo no final do array
-    setTweets([...tweets, "Tweet 5"]);
+    setTweets([...tweets,  "Tweet " + tweetNumber]);
+    setTweetNumber(tweetNumber + 1);
     // --------------------------------------------------------
     // Para exemplificar, pode ser criado um array diferente e adicioná-lo ao estado
     // let string = ["banana", "abacate", "abóbora","morango"];
@@ -33,7 +30,17 @@ function App() {
         return <Tweet text={tweet} />;
       })}
       <p>Fim da lista!</p>
-      <button onClick={createTweet}>Adicionar Tweet</button>
+      <button
+        onClick={createTweet}
+        style={{
+          background: "#8257e6",
+          border: 8,
+          padding: "6px 1px",
+          color: "#FFF",
+        }}
+      >
+        Adicionar Tweet
+      </button>
     </div>
   );
 }
