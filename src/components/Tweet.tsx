1957446702481b2b724e7d147c278533f8ab79f8
kayque-willy@hotmail.com <kayque-willy@hotmail.com>
2022-12-13 10:12:29 -0300

    Criação da remoção

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,16 +1,16 @@
 export type TweetProps = {
-    id : number;
-    title?: string;
-    text: string;
-}
+  id: number;
+  title?: string;
+  text: string;
+};
 
 export function Tweet(props: TweetProps) {
-    return (
-        <div>
-            <h3>{props.title}</h3>
-            <p>{props.text}</p>
-            <p>Texto adicional...</p>
-            <hr></hr>
-        </div>
-    );
+  return (
+    <div>
+      <h2>{props.id}</h2>
+      <h3>{props.title}</h3>
+      <p>{props.text}</p>
+      <p>Texto adicional...</p>
+    </div>
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,24 +9,27 @@ function Home() {
   const [tweets, setTweets] = useState<TweetProps[]>([
     {
       id: 1,
-      title: "Título do Tweet",
-      text: "Texto do Tweet",
+      title: "Título do Tweet 1",
+      text: "Texto do Tweet 2",
+    },
+    {
+      id: 2,
+      title: "Título do Tweet 2",
+      text: "Texto do Tweet 2",
     },
   ]);
   // Constante de controle do número do tweet
   var [tweetNumber, setTweetNumber] = useState(2);
 
   function createTweet() {
-    // O setState apaga o antigo estado e adiciona um novo
-    //Copia os tweets anteriores e adiciona o novo no final do array
-    // setTweets([...tweets, "Tweet " + ++tweetNumber]);
     let newTweet = {
-      id: 2,
-      title: "Título do Tweet 2",
-      text: "Texto do Tweet 2",
+      id: ++tweetNumber,
+      title: "Título do Tweet " + tweetNumber,
+      text: "Texto do Tweet " + tweetNumber,
     };
 
-    // setTweets([...tweets, "Tweet " + ++tweetNumber]);
+    // O setState apaga o antigo estado e adiciona um novo
+    //Copia os tweets anteriores e adiciona o novo no final do array
     setTweets([...tweets, newTweet]);
     setTweetNumber(tweetNumber);
     // --------------------------------------------------------
@@ -35,13 +38,31 @@ function Home() {
     // setTweets(string);
   }
 
+  function removeTweet(id: number) {
+    const index = tweets.findIndex((tweet) => {
+      return tweet.id === id;
+    });
+
+    if (index !== -1) {
+      tweets.splice(index, 1);
+      setTweets([...tweets]);
+      setTweetNumber(--tweetNumber);
+    }
+  }
+
   // Renderiza a pagina
   return (
     <div>
       <h1>App React TypeScript</h1>
       <h2>Tweeter</h2>
       {tweets.map((tweet) => {
-        return <Tweet {...tweet} />;
+        return (
+          <>
+            <Tweet {...tweet} />
+            <button onClick={() => removeTweet(tweet.id)}>remover</button>
+            <hr></hr>
+          </>
+        );
       })}
       <p>Fim da lista!</p>
       <button className="buttonTweet" onClick={createTweet}>
