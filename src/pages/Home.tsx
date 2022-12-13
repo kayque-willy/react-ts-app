faa3a75d6ce754e57c5b2a493d0175552deb2028
kayque-willy@hotmail.com <kayque-willy@hotmail.com>
2022-12-13 16:26:27 -0300

    Remoção de métodos não usados

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,20 +5,6 @@ import { useLocation, useNavigate } from "react-router-dom";
 import "../assets/Button.css";
 
 function Home(props: TweetProps) {
-  // const [tweets, setTweets] = useState<TweetProps[]>([
-  //   {
-  //     id: 1,
-  //     title: "Título do Tweet 1",
-  //     text: "Texto do Tweet 2",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "Título do Tweet 2",
-  //     text: "Texto do Tweet 2",
-  //   },
-  // ]);
-  // const [tweets, setTweets] = useState<string[]>(["Tweet 1", "Tweet 2"]);
-  
   // Navegação
   let navigate = useNavigate();
   
@@ -28,31 +14,12 @@ function Home(props: TweetProps) {
   // Cria um array de string que representa os tweets
   const [tweets, setTweets] = useState<TweetProps[]>(state ? state : []);
   
-  // Constante de controle do número do tweet
-  var [tweetNumber, setTweetNumber] = useState(2);
-
+  // Navega pro formulário
   function goForm() {
-    // navigate("/edit");
     navigate("/edit", { state: tweets });
   }
 
-  function createTweet() {
-    let newTweet = {
-      id: ++tweetNumber,
-      title: "Título do Tweet " + tweetNumber,
-      text: "Texto do Tweet " + tweetNumber,
-    };
-
-    // O setState apaga o antigo estado e adiciona um novo
-    //Copia os tweets anteriores e adiciona o novo no final do array
-    setTweets([...tweets, newTweet]);
-    setTweetNumber(tweetNumber);
-    // --------------------------------------------------------
-    // Para exemplificar, pode ser criado um array diferente e adicioná-lo ao estado
-    // let string = ["banana", "abacate", "abóbora","morango"];
-    // setTweets(string);
-  }
-
+  // Remove Tweet
   function removeTweet(id: number) {
     const index = tweets.findIndex((tweet) => {
       return tweet.id === id;
@@ -61,7 +28,6 @@ function Home(props: TweetProps) {
     if (index !== -1) {
       tweets.splice(index, 1);
       setTweets([...tweets]);
-      setTweetNumber(--tweetNumber);
     }
   }
 
@@ -80,7 +46,6 @@ function Home(props: TweetProps) {
         );
       })}
       <p>Fim da lista!</p>
-      {/* <button className="buttonTweet" onClick={createTweet}></button> */}
       <button className="buttonTweet" onClick={goForm}>
         Adicionar Tweet
       </button>
