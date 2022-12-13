d1ec0b30fc4fd67912478524f3bb809df3f9d044
kayque-willy@hotmail.com <kayque-willy@hotmail.com>
2022-12-13 16:27:23 -0300

    Remoção da props não usada

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 
 import "../assets/Button.css";
 
-function Home(props: TweetProps) {
+function Home() {
   // Navegação
   let navigate = useNavigate();
   
