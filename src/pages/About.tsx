39541046e9ec607d128ec588daf365a75685e817
kayque oliveira <kayque-willy@hotmail.com>
2023-04-18 12:35:17 -0300

    Html structure

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,15 +3,19 @@ import Message from "../components/Message";
 export function About() {
   // Renderiza o componente passando o props por parâmetro
   return (
-    <>
-      <h1>App - React TypeScript</h1>
-      <Message
-        id={1}
-        title={"Twitter"}
-        text={
-          "Exemplo de aplicativo React criado com Vite usando Typescript,Armazenamento local e Routes"
-        }
-      />
-    </>
+    <main>
+      <header>
+        <h1>App - React TypeScript</h1>
+      </header>
+      <section>
+        <Message
+          id={1}
+          title={"Twitter"}
+          text={
+            "Exemplo de aplicativo React criado com Vite usando Typescript, Hooks, Armazenamento local e Routes"
+          }
+        />
+      </section>
+    </main>
   );
 }
