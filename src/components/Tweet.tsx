35bd323e964ab7a3fc1ee7a44ff1df3c3242d368
kayque oliveira <kayque-willy@hotmail.com>
2023-04-25 00:01:13 -0300

    Severe changes, form change to home, css change

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -7,7 +7,7 @@ export type TweetProps = {
 export function Tweet(props: TweetProps) {
   return (
     <div key={props.id}>
-      <h3>{props.id} - {props.title}</h3>
+      {props.title && <span>Author {props.title}</span>}
       <p>{props.text}</p>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,14 @@ import { Tweet, TweetProps } from "../components/Tweet";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
-import { TweetForm } from "./TweetForm";
 
-function Home() {
+export default function Home() {
+
+  // Campos editáveis 
+  const [edit, setEdit] = useState(false);
+  const [text, setText] = useState<string>("");
+  const [id, setId] = useState<number>();
+
   // Navegação
   let navigate = useNavigate();
 
@@ -17,23 +22,27 @@ function Home() {
     getTweets ? JSON.parse(getTweets) : []
   );
 
-  //Armazena o array a cada modificação no componente
+  //Armazena o array a cada modificação no componente tweets
   useEffect(() => {
+    console.log("useEffect");
     localStorage.setItem("tweets", JSON.stringify(tweets));
-  });
+  }, [tweets]);
 
-  // Navega pro formulário
-  function goForm() {
-    navigate("/edit");
+  // Navega pra página About
+  function goAbout() {
+    navigate("/about");
   }
 
   // Edit Tweet
-  function editTweet(tweet: TweetProps) {
-    navigate("/edit", { state: tweet });
+  const editTweet = (tweet: TweetProps) => {
+    setId(tweet.id);
+    setText(tweet.text);
+    setEdit(true);
+    window.scrollTo(0, 0);
   }
 
   // Remove Tweet
-  function removeTweet(id: number) {
+  const removeTweet = (id: number) => {
     const index = findId(id);
     if (index !== -1) {
       tweets.splice(index, 1);
@@ -49,17 +58,68 @@ function Home() {
     return index;
   }
 
+  // Salva o tweet
+  const submit = (event: any) => {
+    // Necessário para evitar que a submissão do formulário atulize a página
+    event.preventDefault();
+    // Cria novo Tweet
+    if (!edit) {
+      let newTweet = {
+        id: 0,
+        title: 'You',
+        text: text,
+      };
+      let newList = [];
+      let lenght = Object.keys(tweets).length;
+
+      if (lenght > 0) {
+        let lastElement = tweets.slice(-1);
+        newTweet.id = lastElement[0].id + 1;
+        newList = [...tweets, newTweet];
+      } else {
+        newTweet.id = 1;
+        newList = [newTweet];
+      }
+      setTweets(newList);
+      // Edita um Tweet 
+    } else {
+      let index = findId(id ? id : -1);
+
+      if (index !== -1) {
+        tweets[index].text = text ? text : "";
+        tweets[index].title = 'You';
+      }
+      setTweets(tweets);
+      setId(-1);
+      setText("");
+      setEdit(false);
+    }
+  }
+
   // --------------------------- Renderiza a pagina ---------------------------
   return (
     <main>
-      {/* <button className="buttonAdd" onClick={goForm}>
-        Adicionar Tweet
-      </button> */}
-      <TweetForm />
+      <section className="form-bg">
+        <form onSubmit={event => { submit(event) }}>
+          <fieldset>
+            <textarea
+              id="text"
+              name="text"
+              value={text}
+              onChange={(event) => setText(event.target.value)}
+              maxLength={200}
+              placeholder="No que você esta pensando?"
+              required
+            />
+          </fieldset>
+          <button type="submit" value="submit">{edit ? 'Editar Tweet' : 'Tweetar'}</button>
+        </form>
+      </section>
+
       <section>
-        {tweets.slice(0).reverse().map((tweet) => {
+        {tweets.slice(0).reverse().map((tweet, index) => {
           return (
-            <article>
+            <article key={index}>
               <Tweet {...tweet} />
               <button className="buttonEdit" onClick={() => editTweet(tweet)}>editar</button>
               <button className="buttonRemove" onClick={() => removeTweet(tweet.id)}>
@@ -73,5 +133,3 @@ function Home() {
     </main>
   );
 }
-
-export default Home;
